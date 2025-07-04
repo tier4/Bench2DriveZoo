7e0f307d9ecf454fe4272c9a1a7a30e84d446ad5
Max-Bin <vborisw@gmail.com>
2025-07-04 17:05:27 +0900

    imporved py310 support

--- mmcv/datasets/pipelines/data_augment_utils.py
+++ mmcv/datasets/pipelines/data_augment_utils.py
@@ -2,7 +2,10 @@
 import numba
 import numpy as np
 import warnings
-from numba.errors import NumbaPerformanceWarning
+try:
+    from numba.core.errors import NumbaPerformanceWarning
+except ImportError:
+    from numba import NumbaPerformanceWarning
 
 from mmcv.core.bbox import box_np_ops
 
